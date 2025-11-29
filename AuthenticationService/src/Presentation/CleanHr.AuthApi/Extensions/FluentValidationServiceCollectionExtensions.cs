e68ec2124b9e1eb7c4b3c214a978ccd2b9502563
Tanvir Ahmad Arjel <tanvirarjel@TanvirMac.local>
2025-11-29 15:27:25 +0700

    feat: UserId added to email verification code.

--- AuthenticationService/src/Presentation/CleanHr.AuthApi/Extensions/FluentValidationServiceCollectionExtensions.cs
+++ AuthenticationService/src/Presentation/CleanHr.AuthApi/Extensions/FluentValidationServiceCollectionExtensions.cs
@@ -8,9 +8,5 @@ internal static class FluentValidationServiceCollectionExtensions
     public static void AddFluentValidation(this IServiceCollection services)
     {
         ValidatorOptions.Global.DefaultRuleLevelCascadeMode = CascadeMode.Stop;
-
-        // Make sure this is from SharpGrip.FluentValidation.AutoValidation.Mvc.Extensions;
-        // Otherwise Async validation would not work.
-        services.AddFluentValidationAutoValidation();
     }
 }
